0f2ee7befcc782e8fb5a0a1b7b5ea8419bdd1f08
Sachico <s104103045@gmail.com>
2025-07-27 16:58:24 +0800

    v-for、v-if、v-else、v-show

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,56 @@
 <script setup>
 import { ref } from 'vue'
-// v-on: 像監聽器的魔法?
-// 縮寫 @
 
-const handleClick = ()=>{
-  console.log("我被觸發了")
-}
+//v-for
+//v-if
+//v-show
 
-const submitForm = ()=>{
-  console.log("表單被觸發了")
-}
+const array = ref([
+  {
+  id:'1',
+  name: '小明',
+  age: 18 
+  },
+  {
+  id:'2',
+  name: '小美',
+  age: 16 
+  },
+  {
+  id:'3',
+  name: '大仁',
+  age: 20 
+  },
+])
 
-const text = ref('這是一段文字')
+const isChecked = ref(false)
 
 </script>
 
 <template>
-  <div><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-    <form action="" @submit.prevent="submitForm">
-    <button type="button" v-on:click="handleClick">type="button"按鈕</button>
-    <p>當按鈕存在於表單內時，預設行為會送出並刷新頁面，所以需要使用type="button"</p>
-    <button @click="handleClick">沒有type的按鈕</button>
-    <button type="submit">type="submit"按鈕></button>
-    </form>
+  <div>
+    <div>
+      姓名:{{ array[0].name }}
+      年齡:{{ array[0].age }}
+    </div>
+    <hr>
+    // v-for 必須加上:key
+    <div v-for="(item, key) in array" :key="item.id">
+      索引:{{ key }}
+      姓名:{{ item.name }}
+      年齡:{{ item.age }}
+      <div v-if="item.age > 18">大於 18 歲</div>
+      <div v-else>小於 18 歲</div>
+      
+    </div>
+    <hr>
+    <input type="checkbox" v-model="isChecked">{{ isChecked }}
+    
+    <b v-if="isChecked">當 isChecked == true 會顯示這行</b>
+    <b v-else="isChecked">目前的 isChecked == false</b>
+    <hr>
+    <b v-show="isChecked">v-show 目前的 isChecked == true</b>
 
-    <h2>事件修飾符</h2>
-    <a href="#" @click="handleClick">我是連結</a>
-    <p>a連結#會跳到特定id位置，如果沒有特定位置就會回到最上面</p>
-    <a href="#" @click.prevent="handleClick">我是連結清除預設行為</a>
-
-    <h2>按鍵修飾符</h2>
-    <input type="text" @keydown.enter="handleClick" v-model="text">
   </div>
 </template>
 <style>
