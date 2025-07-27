61ea52ae607624f1bc09a47cdb14a683fd2125bd
Sachico <s104103045@gmail.com>
2025-07-27 14:49:41 +0800

    雙向綁定

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,37 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// 雙向綁定
+const text = ref('這是一段文字')
+const city = ref('台北市')
+const isChecked = ref(true)
+const arrayCheckbox = ref([])
+const radioValue = ref('男')
 </script>
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>{{ text }}</h1>
+    <input type="text" v-model="text">
+    <hr>
+    {{ city }}
+    <select name="" id="" v-model="city">
+      <option value="0">台北市</option>
+      <option value="1">台中市</option>
+      <option value="2">高雄市</option>
+    </select>
+    <hr>
+    {{ isChecked }}
+    <input type="checkbox" v-model="isChecked">
+    <hr>
+    {{ arrayCheckbox }}
+    <input type="checkbox" v-model="arrayCheckbox" value="小姐姐">
+    <input type="checkbox" v-model="arrayCheckbox" value="小哥哥">
+    <input type="checkbox" v-model="arrayCheckbox" value="漂亮阿姨">
+    <hr>
+    {{ radioValue }}
+    <input type="radio" v-model="radioValue" value="南">
+    <input type="radio" v-model="radioValue" value="女">
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
