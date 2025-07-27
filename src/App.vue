d678a55f17a454d0013d46658277d3613ea84f1e
Sachico <s104103045@gmail.com>
2025-07-27 17:48:20 +0800

    try something else with v-model

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,60 @@
 <script setup>
 import { ref } from 'vue'
 
-//v-for
-//v-if
-//v-show
+const message = ref('這是一串訊息')
 
-const array = ref([
-  {
-  id:'1',
-  name: '小明',
-  age: 18 
-  },
-  {
-  id:'2',
-  name: '小美',
-  age: 16 
-  },
-  {
-  id:'3',
-  name: '大仁',
-  age: 20 
-  },
-])
+const number1 = ref(1)
 
-const isChecked = ref(false)
+const number2 = ref('2')
+
+const checkboxArray = ref([])
+
+const singleRadio = ref([])
 
 </script>
 
 <template>
   <div>
-    <div>
-      姓名:{{ array[0].name }}
-      年齡:{{ array[0].age }}
+    <input type="text" v-model="message">
+    {{ message }}
+    <hr>
+    <input type="text" v-model.lazy="message">
+    {{ message }}
+    <hr>
+    <input type="text" v-model.trim="message">
+    {{ message }}
+    <hr>
+    <input type="text" v-model.number="number1">
+    {{ number1 }}
+    {{ typeof number1 }}
+    <hr>
+    <input v-model="number1">
+    {{ number1 }}
+    {{ typeof number1 }}
+    <hr>
+    <input type="text" v-model="number2">
+    {{ number2 }}
+    {{ typeof number2 }}
+    <hr>
+    {{ checkboxArray }}
+    <div class="form-check">
+      <input type="checkbox" class="form-check-input" id="check1" v-model="checkboxArray" value="雞">
+      <label class="form-check-label" for="check1">雞</label>
+      <input type="checkbox" class="form-check-input" id="check2" v-model="checkboxArray" value="豬">
+      <label class="form-check-label" for="check2">豬</label>
+      <input type="checkbox" class="form-check-input" id="check3" v-model="checkboxArray" value="牛">
+      <label class="form-check-label" for="check3">牛</label>
     </div>
     <hr>
-    // v-for 必須加上:key
-    <div v-for="(item, key) in array" :key="item.id">
-      索引:{{ key }}
-      姓名:{{ item.name }}
-      年齡:{{ item.age }}
-      <div v-if="item.age > 18">大於 18 歲</div>
-      <div v-else>小於 18 歲</div>
-      
+    您選擇了:{{ singleRadio }}
+    <div class="form-check">
+      <input type="radio" class="form-check-input" id="radio1" v-model="singleRadio" value="雞">
+      <label class="form-check-label" for="radio1">雞</label>
+      <input type="radio" class="form-check-input" id="radio2" v-model="singleRadio" value="豬">
+      <label class="form-check-label" for="radio2">豬</label>
+      <input type="radio" class="form-check-input" id="radio3" v-model="singleRadio" value="牛">
+      <label class="form-check-label" for="radio3">牛</label>
     </div>
-    <hr>
-    <input type="checkbox" v-model="isChecked">{{ isChecked }}
-    
-    <b v-if="isChecked">當 isChecked == true 會顯示這行</b>
-    <b v-else="isChecked">目前的 isChecked == false</b>
-    <hr>
-    <b v-show="isChecked">v-show 目前的 isChecked == true</b>
 
   </div>
 </template>
