8ba4bbd1f15cc04ef8b87b1263052b2ad57e1ddb
Sachico <s104103045@gmail.com>
2025-07-27 16:20:52 +0800

    v-on

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,39 @@
 <script setup>
 import { ref } from 'vue'
+// v-on: 像監聽器的魔法?
+// 縮寫 @
 
-const imgUrl = ref('https://images.unsplash.com/photo-1682685796063-d2604827f7b3?q=80&w=300&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')
-const isChecked = ref(false)
+const handleClick = ()=>{
+  console.log("我被觸發了")
+}
+
+const submitForm = ()=>{
+  console.log("表單被觸發了")
+}
+
+const text = ref('這是一段文字')
+
 </script>
 
 <template>
-  <div>
-   <img v-bind:src="imgUrl" alt="">
-   <h2>v-bind:可以套用在任何屬性上</h2>
-   <h2>:黑魔法?</h2>
-   {{ isChecked }}
-   <input type="checkbox" v-model="isChecked">
-   <input type="text" :disabled="isChecked">
-   <h2>可以套用在行內樣式上</h2>
-  <div 
-   class="box" 
-   v-bind:style="{ transform: isChecked ? 'rotate(45deg)': null }">
-  </div>
-  <hr>
-  <div 
-   class="box" 
-   v-bind:style="{ backgroundColor: isChecked ? 'red' : 'green' }">
-  </div>
-  <h2>class</h2>
-  <div class="box" :class="{ rotate : isChecked }"></div>
+  <div><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <form action="" @submit.prevent="submitForm">
+    <button type="button" v-on:click="handleClick">type="button"按鈕</button>
+    <p>當按鈕存在於表單內時，預設行為會送出並刷新頁面，所以需要使用type="button"</p>
+    <button @click="handleClick">沒有type的按鈕</button>
+    <button type="submit">type="submit"按鈕></button>
+    </form>
 
+    <h2>事件修飾符</h2>
+    <a href="#" @click="handleClick">我是連結</a>
+    <p>a連結#會跳到特定id位置，如果沒有特定位置就會回到最上面</p>
+    <a href="#" @click.prevent="handleClick">我是連結清除預設行為</a>
+
+    <h2>按鍵修飾符</h2>
+    <input type="text" @keydown.enter="handleClick" v-model="text">
   </div>
 </template>
 <style>
 
-.box {
-  height: 100px;
-  width: 100px;
-  background-color: red;
-  transition: .5s all;
-}
 
-.rotate {
-  transform: rotate(45deg);
-}
 </style>
